16f5f3b7d09760c7d0e603c5ad31d5d89b1ef57b
Pavel Korolev <dev@borodust.org>
2017-10-19 23:17:11 +0300

    Make flashlight follow a cursor

--- main.lisp
+++ main.lisp
@@ -4,18 +4,29 @@
 (defvar *black* (vec4 0 0 0 1))
 (defvar *white* (vec4 1 1 1 1))
 
+(defvar *look-at* 0.0)
+
 (defclass notalone (gamekit:gamekit-system)
   ()
   (:default-initargs
     :viewport-width 800
-    :viewport-height 600))
+    :viewport-height 600
+    :viewport-title "notALone"))
+
+
+(defun look-at (x y)
+  (setf *look-at* (- (atan (- y 300) (- x 400)) (/ pi 2) )))
+
+
+(defmethod initialize-host :after ((this notalone))
+  (bind-cursor #'look-at))
 
 
 (defmethod draw ((this notalone))
   (draw-rect (vec2 0 0) 800 600 :fill-paint *black*)
   (ge.vg:with-pushed-canvas ()
     (ge.vg:translate-canvas 400 300)
-    (ge.vg:rotate-canvas (* (mod (* 100 (ge.util:epoch-seconds)) 360) (/ pi 180)))
+    (ge.vg:rotate-canvas *look-at*)
     (draw-flashlight)))
 
 
@@ -25,8 +36,8 @@
         (radius 250)
         (aa-delta 1.0))
     (draw-arc (vec2 0 0) radius start-angle end-angle :fill-paint *white*)
-    (draw-polygon (list (vec2 -25 0) (vec2 25 0)
+    (draw-polygon (list (vec2 -5 0) (vec2 5 0)
                         (vec2 (* radius (cos start-angle)) (+ aa-delta (* radius (sin start-angle))))
                         (vec2 (* radius (cos end-angle)) (+ aa-delta (* radius (sin end-angle)))))
                   :fill-paint *white*)
-    (draw-arc (vec2 0 -95) 100 start-angle end-angle :fill-paint *black*)))
+    (draw-arc (vec2 0 -9) 10 start-angle end-angle :fill-paint *black*)))
